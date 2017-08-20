23f0dcd5f2d7255c43e648db84576a39e1325423
Parthiban, Arun <arun.parthiban@deluxe.com>
2017-08-19 20:48:19 -0500

    Adding desciption to setup tools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='RancherDeploy',
       version='0.1',
-      description='The funniest joke in the world',
+      description='A script to deploy services to Rancher',
       packages=['RancherDeploy'],
       install_requires=['requests', 'click'],
       entry_points = { 'console_scripts' : ['RancherDeploy=RancherDeploy:main']},
